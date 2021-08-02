d1f7a65f7f7b69754cb4d722c1c9c2ff94bd1889
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2021-08-03 01:30:21 +0530

    Simple Calculator.

---
+++ calculator/calc.js
@@ -0,0 +1,97 @@
+// to check whether the function is leap year or not
+/*let year="2016"
+if(year%4==0){
+    if(year%100==0){
+        if(year%400==0){
+            console.log(`${year} is a leap year`)
+        }else{
+            console.log(`${year} is not a leap year`)
+        }
+    }else{
+        console.log(`${year} is a leap year`)
+    }
+}else{
+    console.log(`${year} is not a leap year`)
+}
+
+
+//for in loop
+var a=['yash','mummy','vanshu','papa']
+for(let b in a){
+    console.log(`the indices of array is ${b}`)
+}
+
+//for of loop
+var b=['google','microsoft','facebook','amazon']
+for(let c of b){
+    console.log(`i will work as engineer in ${c}`)
+}
+
+//forEach loop
+var c=['sde','devops','web developer','machine learning']
+c.forEach(function(elements,index,array){
+    console.log(`i am ${elements} which is on ${index} position among ${array}`)
+});
+
+//forEach using arrow function
+var c=['sde','devops','web developer','machine learning']
+c.forEach((elements,index,array)=>{
+    console.log(`i am ${elements} which is on ${index} position among ${array}`)
+})
+
+// new date()
+let currDate=new Date()
+console.log(currDate);
+console.log(Date.now());
+console.log(currDate.setHours(6));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.3));
+console.log(Math.trunc(5.675,2));
+
+---map method
+
+let arr = [2,3,4,5,6]
+let arr1 = arr.map((curElem)=>{
+    return curElem*2;
+}).filter((curElem)=>{
+    return curElem > 10
+});
+console.log(arr1);
+
+----reduce method
+
+let a = [2,3,4,5,6]
+let b = a.map((elem)=>{return elem*2} ).filter((elem)  => {return elem>=5}).reduce((acc,elem)=>{return acc +=elem});
+console.log(b);
+
+---calculator
+
+const add = (a,b) => {
+    return a+b
+}
+const subs = (a,b) => {
+    return math.abs(a-b)
+}
+const prod = (a,b) => {
+    return a*b
+}
+const divide = (a,b) => {
+    return a/b
+}
+const per = (a,b) => {
+    return a/b*100
+}
+const pow = (a,b) => {
+    return a**b
+}
+
+const operator = (c,d,operator) => {
+    return operator(c,d)
+}
+document.write(operator(10,30,pow))
+*/
+
+
+
+
+
